8d7490ba7be361d6f057afe5ce7721ea7c0111ba
CAMR05 <cmoheno@centro.edu.mx>
2025-12-08 14:40:08 -0600

    configuración en vite config para subir todas las páginas al link público de Vercel

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,15 @@ export default {
     {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input:{
+                main: 'src/index.html', // Main entry point
+                page2: 'src/cars.html', // Second entry point
+                page3: 'src/cassettes.html',// Third entry point
+                page4: 'src/comics.html'// Fourth entry point
+            }
+        }
     },
     plugins:
     [
